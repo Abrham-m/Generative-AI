562f60c142af220151f556e3e796dcca4655546e
Abrham-m <abrhammen14@gmail.com>
2024-04-02 20:31:54 +0300

    login page is done

---
+++ client/src/Components/Login.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import "./Login.css";
+
+const LoginForm = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center w-full dark:bg-gray-950">
+      <div className="bg-white dark:bg-gray-800 shadow-md rounded-lg px-8 py-6 max-w-md">
+        <h1 className="text-2xl font-bold text-center mb-4 dark:text-gray-200 ">
+          Login
+        </h1>
+        <form action="#">
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">
+              Email
+            </label>
+            <input
+              type="text"
+              placeholder="Enter your Email"
+              required
+              className="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-700"
+            />
+          </div>
+
+          <div className="mb-5">
+            <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Enter your Password"
+              required
+              className="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-700"
+            />
+            <a
+              href="reference"
+              class="text-sm text-gray-500 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Forgot Password?
+            </a>
+          </div>
+          <div className="flex items-center justify-between mb-4">
+            <div className="flex items-center">
+              <input
+                type="checkbox"
+                id="remember"
+                className="w-4 h-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500 focus:outline-none"
+                checked
+              />
+              <label
+                for="remember"
+                className="ml-2 block text-sm text-gray-700 dark:text-gray-300"
+              >
+                Remember me
+              </label>
+            </div>
+            <a
+              href="ref"
+              className="text-xs text-indigo-500 hover:text-indigo-700 focus:outline-none focus:ring-2 focus:offset-2 focus:ring-indigo-500"
+            >
+              Create Account
+            </a>
+          </div>
+          <button
+            type="submit"
+            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
