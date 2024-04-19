2de151cf4faee9d74f242bf95984b01755ddad11
Abrham-m <abrhammen14@gmail.com>
2024-04-19 10:50:46 +0300

    login theme is fixed

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-// import "./Login.css";
+import { Link } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 const LoginForm = () => {
   return (
-    <div className="min-h-screen flex justify-center items-center w-full dark:bg-gray-950">
-      <div className="bg-white dark:bg-gray-800 shadow-md rounded-lg px-8 py-6 max-w-md">
-        <h1 className="text-2xl font-bold text-center mb-4 dark:text-gray-200 ">
+    <div className="min-h-screen flex justify-center items-center w-full bg-gray-950">
+      <div className="bg-gray-800 shadow-md rounded-lg px-8 py-6 max-w-md">
+        <h1 className="text-2xl font-bold text-center mb-4 text-gray-200 ">
           Login
         </h1>
         <form action="#">
           <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">
+            <label className="block text-sm font-medium text-gray-300 mb-2">
               Email
             </label>
             <input
@@ -22,7 +24,7 @@ const LoginForm = () => {
           </div>
 
           <div className="mb-5">
-            <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">
+            <label className="block text-sm font-medium text-gray-300  mb-2">
               Password
             </label>
             <input
@@ -44,21 +46,21 @@ const LoginForm = () => {
                 type="checkbox"
                 id="remember"
                 className="w-4 h-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500 focus:outline-none"
-                checked
+                defaultChecked
               />
               <label
                 for="remember"
-                className="ml-2 block text-sm text-gray-700 dark:text-gray-300"
+                className="ml-2 block text-sm text-gray-300 "
               >
                 Remember me
               </label>
             </div>
-            <a
-              href="ref"
+            <Link
+              to="/register"
               className="text-xs text-indigo-500 hover:text-indigo-700 focus:outline-none focus:ring-2 focus:offset-2 focus:ring-indigo-500"
             >
               Create Account
-            </a>
+            </Link>
           </div>
           <button
             type="submit"
@@ -67,6 +69,26 @@ const LoginForm = () => {
             Login
           </button>
         </form>
+        <div class="flex flex-row justify-center my-4">
+          <span class="absolute bg-gray-800 px-4 text-gray-500">
+            or sign-in with
+          </span>
+          <div class="w-full bg-gray-200 mt-3 h-px"></div>
+        </div>
+        <div class="flex flex-row justify-center items-center gap-2">
+          <button className="w-1/2 mx-3 flex justify-center py-3 px-4 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-blueness/[0.7] hover:bg-blueness/[1] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blueness">
+            <span className="mr-2">
+              <FcGoogle size={20} />
+            </span>{" "}
+            Google
+          </button>
+          <button className="w-1/2 mx-3 flex justify-center py-3 px-4 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-blueness/[0.7] hover:bg-blueness/[1] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blueness">
+            <span className="mr-2">
+              <FaGithub size={20} />
+            </span>{" "}
+            GitHub
+          </button>
+        </div>
       </div>
     </div>
   );
